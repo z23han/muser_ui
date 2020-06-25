cc5902334dfef01d799855bc4c120ce8bb7a5601
ohbennn <benjamin689@yahoo.ca>
2020-06-25 00:01:43 -0400

    renamed original homepage to music base, created homepage and the associated service music category

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:muser_ui/routes/home.dart';
+import 'package:muser_ui/routes/music_base.dart';
 import 'package:muser_ui/routes/loader.dart';
 import 'package:muser_ui/routes/music_player.dart';
 import 'package:muser_ui/routes/signin.dart';
@@ -13,10 +14,12 @@ GetIt getIt = GetIt.instance;
 void main() {
   getIt.registerSingleton<FlutterSecureStorage>(FlutterSecureStorage());
   runApp(MaterialApp(
-    theme: ThemeData(primaryColor: Colors.grey[400], accentColor: Colors.grey[800]),
-    initialRoute: '/musicPlayer',
+    theme: ThemeData(
+        primaryColor: Colors.grey[400], accentColor: Colors.grey[800]),
+    initialRoute: '/loader',
     routes: {
       '/': (context) => Home(),
+      '/musicBase': (context) => MusicBase(),
       '/loader': (context) => Loader(),
       '/signin': (context) => SignIn(),
       '/signup': (context) => SignUp(),
