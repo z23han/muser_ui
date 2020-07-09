d5e2c2f1e53d39dbb7bfdcd511c9c23253c72093
z23han <zxhan2010@hotmail.com>
2020-07-09 18:52:27 -0400

    play music from the list

--- lib/routes/music/music_player.dart
+++ lib/routes/music/music_player.dart
@@ -1,6 +1,9 @@
+import 'package:audioplayers/audio_cache.dart';
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:muser_ui/services/music_object.dart';
+import 'dart:io';
 
 class MusicPlayerScreen extends StatefulWidget {
   final Music music;
@@ -13,21 +16,96 @@ class MusicPlayerScreen extends StatefulWidget {
 
 class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
 
-  bool _isPlaying = false;
+  bool _isPlaying = true;
+  AudioCache audioCache = AudioCache();
+  AudioPlayer audioPlayer = AudioPlayer();
+  double _currPosition = 0;
+  double _maxPosition = 60000;
+  final double _minPosition = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    if (Platform.isIOS) {
+      if (audioCache.fixedPlayer != null) {
+        audioCache.fixedPlayer.startHeadlessService();
+      }
+      audioPlayer.startHeadlessService();
+    }
+
+    // init state is play
+    initPlayer();
+    play();
+    _updateSlider();
+  }
+
+  initPlayer() async {
+    int result = await audioPlayer.setUrl(widget.music.url);
+    if (result == 1) {
+      print('${widget.music.name} is ready');
+    }
+
+    int totalMilliSec = 0;
+    while (totalMilliSec == 0 || totalMilliSec == null) {
+      totalMilliSec = await Future.delayed(Duration(seconds: 2), () => 
+        audioPlayer.getDuration()
+      );
+    }
+    print('${widget.music.name} total length in millisec: $totalMilliSec');
+    setState(() {
+      _maxPosition = totalMilliSec.toDouble();
+    });
+  }
+
+  play() async {
+    int result = await audioPlayer.play(widget.music.url);
+    if (result == 1) {
+      print("${widget.music.name} is playing");
+    }
+  }
+
+  pause() async {
+    int result = await audioPlayer.pause();
+    if (result == 1) {
+      print("${widget.music.name} is paused");
+    }
+  }
+
+  stop() async {
+    int result = await audioPlayer.stop();
+    if (result == 1) {
+      print("${widget.music.name} is stopped");
+    }
+  }
+
+  seek(double newValue) async {
+    int result = await audioPlayer.seek(Duration(milliseconds: newValue.toInt()));
+    if (result == 1) {
+      print('${widget.music.name} is set to $newValue in milliseconds');
+    }
+  }
+
+  void _updateSlider() {
+    audioPlayer.onAudioPositionChanged.listen((Duration p) {
+      setState(() {
+        _currPosition = p.inMilliseconds.toDouble();
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
       child: Scaffold(
+        appBar: AppBar(),
         body: SingleChildScrollView(
           child: Column(
             children: <Widget>[
-
               SizedBox(
-                height: 100,
+                height: 30,
               ),
-
               Container(
                 margin: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
                 decoration: BoxDecoration(
@@ -64,10 +142,15 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
                 ),
               ),
               Slider(
-                onChanged: (v) {},
-                value: 10,
-                max: 100,
-                min: 0,
+                value: _currPosition,
+                max: _maxPosition,
+                min: _minPosition,
+                onChanged: (newValue) {
+                  setState(() {
+                    _currPosition = newValue;
+                    seek(newValue);
+                  });
+                },
                 activeColor: Colors.grey[800],
               ),
               SizedBox(height: 30,),
@@ -83,6 +166,11 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
                     onPressed: () {
                       setState(() {
                         _isPlaying = !_isPlaying;
+                        if (_isPlaying) {
+                          play();
+                        } else {
+                          pause();
+                        }
                       });
                     },
                     icon: Icon(
@@ -95,7 +183,6 @@ class _MusicPlayerScreenState extends State<MusicPlayerScreen> {
                   )
                 ],
               )
-
 
             ],
           ),
